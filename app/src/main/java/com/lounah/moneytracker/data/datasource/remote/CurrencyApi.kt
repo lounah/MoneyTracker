5c3d83e64a425f2d8dd296e1fbfcb0082cd7ab2e
Lounah <lounahxrussia@gmail.com>
2018-07-23 03:59:05 +0300

    created api interface

---
+++ app/src/main/java/com/lounah/moneytracker/data/datasource/remote/CurrencyApi.kt
@@ -0,0 +1,9 @@
+package com.lounah.moneytracker.data.datasource.remote
+
+import io.reactivex.Single
+
+interface CurrencyApi {
+
+    fun getExchangeRate(from : String, to : String) : Single<Double>
+
+}
