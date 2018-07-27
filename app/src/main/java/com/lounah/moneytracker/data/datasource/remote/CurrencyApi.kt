d6efca39f5233d69b697aeeb3e0fc02610f8312a
Lounah <lounahxrussia@gmail.com>
2018-07-27 21:17:58 +0300

    api with exchange rates integrated

--- app/src/main/java/com/lounah/moneytracker/data/datasource/remote/CurrencyApi.kt
+++ app/src/main/java/com/lounah/moneytracker/data/datasource/remote/CurrencyApi.kt
@@ -1,7 +1,14 @@
 package com.lounah.moneytracker.data.datasource.remote
 
 import io.reactivex.Single
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CurrencyApi {
-    fun getExchangeRate(from: String, to: String): Single<Double>
+
+    @GET("v6/convert?compact=ultra")
+    fun getExchangeRate(@Query("q") query: String): Call<ResponseBody>
 }
