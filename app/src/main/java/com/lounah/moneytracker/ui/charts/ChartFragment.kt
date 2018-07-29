e0ad251860446a736bb772d84e62960bd218fb58
Lounah <lounahxrussia@gmail.com>
2018-07-30 02:00:02 +0300

    chart fragment init

---
+++ app/src/main/java/com/lounah/moneytracker/ui/charts/ChartFragment.kt
@@ -0,0 +1,31 @@
+package com.lounah.moneytracker.ui.charts
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lounah.moneytracker.ui.MainActivity
+import com.lounah.moneytracker.ui.common.BaseFragment
+import com.lounah.wallettracker.R
+
+class ChartFragment : BaseFragment() {
+    override val layoutRes: Int
+        get() = R.layout.fragment_charts
+
+    override val TAG: String
+        get() = "CHART_FRAGMENT"
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        setUpToolbarTitle(R.string.statistics)
+    }
+
+    override fun setUpToolbarTitle(resId: Int) {
+        (activity as MainActivity).onUpdateToolbarTitle(resId)
+    }
+
+}
