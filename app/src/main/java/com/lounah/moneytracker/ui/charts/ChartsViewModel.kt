4c583664fd30f3f9728a81a4bd2f4524dd5f3730
Lounah <lounahxrussia@gmail.com>
2018-07-30 05:28:08 +0300

    it will be custom chartview

---
+++ app/src/main/java/com/lounah/moneytracker/ui/charts/ChartsViewModel.kt
@@ -0,0 +1,2 @@
+package com.lounah.moneytracker.ui.charts
+
---
+++ app/src/main/java/com/lounah/moneytracker/ui/views/ChartView.kt
@@ -0,0 +1,21 @@
+package com.lounah.moneytracker.ui.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ChartView : View {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attributeSet: AttributeSet)
+            : super(context, attributeSet)
+
+    constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int)
+            : super(context, attributeSet, defStyleAttr)
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int)
+            : super(context, attrs, defStyleAttr, defStyleRes)
+
+
+}
