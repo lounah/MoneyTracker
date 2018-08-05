b1da088ba45da7b16f5a5b1bd12e2c2adb0ecd56
Popov Bogdan <popovbodya@gmail.com>
2018-08-05 16:44:23 +0300

    Поправил показ транзакции

--- app/src/main/kotlin/ru/popov/bodya/presentation/transactions/TransactionViewHolder.kt
+++ app/src/main/kotlin/ru/popov/bodya/presentation/transactions/TransactionViewHolder.kt
@@ -17,10 +17,10 @@ class TransactionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) 
 
     fun bind(item: Transaction) = with(itemView) {
         tv_description.text = item.description
-        tv_time.text = DateFormat.format("hh:mm", item.date)
-        tv_currency.text = item.currency.toString()
+        date_text_view.text = DateFormat.format("dd.MM.yyyy, HH:mm", item.date)
+        currency_text_view.text = item.currency.toString()
         transaction_category_text_view.text = itemView.resources.getString(ResourcesSelector.fromTransactionCategoryToString(item.category))
-        iv_currency_type.setImageResource(ResourcesSelector.fromTransactionCategoryToDrawable(item.category))
+        currency_type_image_view.setImageResource(ResourcesSelector.fromTransactionCategoryToDrawable(item.category))
 
         if (item.category is TransactionsCategory.Expense) {
             tv_amount.setBaseColor(itemView.resources.getColor(R.color.colorExpense))
